415ded4862456651908a06dc2b8621857ef93096
Ali Yagmur <aliyagmur@gmail.com>
2022-11-04 16:44:01 +0300

    modifications

--- calculator/app/src/main/java/com/yagmurali/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/yagmurali/calculator/MainActivity.kt
@@ -2,10 +2,75 @@ package com.yagmurali.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView? = null;
+
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
+
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+
+    }
+    fun onClear(view: View) {
+        tvInput?.setText("")
+    }
+
+    fun onDecimalPoint(view: View) {
+        // if (tvInput?.text?.contains(".") == false) {
+            if (lastNumeric && !lastDot) {
+                tvInput?.append(".")
+                lastNumeric = false
+                lastDot = true
+            }
+        // }
+    }
+
+    fun onOperator(view : View) {
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    fun isOperatorAdded(value: String) : Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/") ||
+                    value.contains("*") ||
+                    value.contains("+") ||
+                    value.contains("-")
+
+        }
+
+    }
+
+    fun onEquals(view : View) {
+        val mgr: ScriptEngineManager = ScriptEngineManager()
+        val engine: ScriptEngine = mgr.getEngineByName("JavaScript")
+        println(tvInput?.text.toString())
+        tvInput?.setText(engine.eval(tvInput?.text.toString()).toString())
+
+    }
+
 }
